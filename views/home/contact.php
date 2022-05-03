d2299952c546148354e78ef862c3ef920a4efa45
James Park <87226158+JamesPark89@users.noreply.github.com>
2022-05-03 22:55:34 +1000

    Edit contact page
    -Edit contact page

--- views/home/contact.php
+++ views/home/contact.php
@@ -6,30 +6,17 @@
   </head>
   <body>
   <?php include '../partials/nav.php';?>
-    <div class="title d-flex justify-content-center">
-      <span>Contact</span>
-    </div>
-    <div class="icons">
-      <div class="emailbox">
-        <img src="../../source/mailicon.png" width="100" height="100" alt="" id="mailicon">
-        <div id="email">
-         mel.jamespark@gmail.com
+    <div class="contents">
+      <div class="contact justify-content-center">
+        <div class="title d-flex justify-content-center">
+        <span>Contact</span>
         </div>
       </div>
-      <a href="https://www.youtube.com/channel/UCfKexLVfgR8gnP5JUdWq2uw" target="_blank">
-        <img src="../../source/youtubeicon.png" width="100" height="100" alt="">
-      </a>
-      <a href="https://www.instagram.com/james_park_89/" target="_blank">
-        <img src="../../source/instaicon.png" width="100" height="100" alt="">
-      </a>
+      <div class="emailbox">
+        <h3>For enquieries please email:</h3> <br/>
+        <h4>Mel.JamesPark@gmail.com</h4>
+      </div>
     </div>
-    <script>
-      const mailicon = document.getElementById('mailicon');
-      mailicon.addEventListener('click',(e)=>{
-        const email = document.getElementById('email');
-        (email.style.display == 'none') ? ((email.style.display = 'block') & (mailicon.style.display = 'none')) : (email.style.display = 'none');
-      })
-    </script>
   </body>
   <!-- footer -->
   <?php include '../partials/footer.php';?>
